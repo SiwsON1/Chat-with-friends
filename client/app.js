421dd8e1ac1ba45b203bda7251b564d530e1df61
Marcin Siwonia <mahinek12@gmail.com>
2023-05-30 18:10:46 +0200

    Add first sockets

--- client/app.js
+++ client/app.js
@@ -1,3 +1,5 @@
+const socket = io();
+
 const loginForm = document.getElementById("welcome-form");
 const messagesSection = document.getElementById("messages-section");
 const messagesList = document.getElementById("messages-list");
@@ -8,10 +10,15 @@ const messageContentInput = document.getElementById("message-content");
 let userName = ''; 
 addMessageForm.autocomplete = "off";
 
+socket.on('message', ({ author, content }) => addMessage(author, content));
+socket.on('join', ({userName}) => addMessage('chatBot', `${userName} has joined the conversation!`));
+socket.on('removeUser', ({userLogin}) => addMessage('chatBot', `${userLogin.userName} has left the conversation... :(`));
+
 function login(e) {
   e.preventDefault();
   if (userNameInput.value !== '') {
     userName = userNameInput.value; 
+    socket.emit('join', { userName: userName });
     loginForm.classList.remove("show");
     messagesSection.classList.add("show");
   } else {
@@ -30,6 +37,7 @@ function sendMessage(e) {
     e.preventDefault();
     if (messageContentInput.value !== '') { 
       addMessage(userName, messageContentInput.value);
+      socket.emit('message', { author: userName, content: messageContentInput.value });
       messageContentInput.value ='';
     }else {
       console.log("Wpisz swoją wiadomość"); 
@@ -44,6 +52,8 @@ function sendMessage(e) {
 
     if(author === userName){
       message.classList.add('message--self');
+    } else if(author === 'chatBot'){
+      message.classList.add('message--chatBot');
     };
     message.innerHTML = `
     <h3 class="message__author">${userName === author ? 'You' : author }</h3>
@@ -57,3 +67,5 @@ function sendMessage(e) {
 addMessageForm.addEventListener("submit", function(e) {
     sendMessage(e);
   });
+
+ 
--- server.js
+++ server.js
@@ -1,15 +1,45 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const socket = require('socket.io');
+
 
 app.use(express.static(path.join(__dirname, 'client')));
 
-const messages = ['msg 1', 'msg 2', 'msg 3'];
+const messages = [];
+const users = [];
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/client/index.html'));
   });
 
-app.listen(9000, () => {
-  console.log('Server is running on port: 8000');
-});
+  const server = app.listen(9001, () => {
+    console.log('Server is running on Port:', 9001)
+  });
+
+  const io = socket(server);
+
+  io.on('connection', (socket) => {
+    console.log('New client! Its id – ' + socket.id);
+    socket.on('message', (message) => { console.log('Oh, I\'ve got something from ' + socket.id);
+    messages.push(message);
+    socket.broadcast.emit('message', message);
+   });
+   socket.on('join', (join) => { console.log('I\'ve add ' + socket.id);
+   const socketId = socket.id;
+     users.push({ join, socketId});
+     socket.broadcast.emit('join', join);
+     console.log(join);
+   });
+    socket.on('disconnect', () => { console.log('Oh, socket ' + socket.id + ' has left')
+    console.log('I\'ve added a listener on message and disconnect events \n');
+    const userIndex = users.findIndex(user => user.socketId === socket.id);
+  if (userIndex !== -1) {
+    const userLogin = users[userIndex].join;
+    users.splice(userIndex, 1);
+    socket.broadcast.emit('removeUser', { userLogin });
+    console.log(userLogin);
+  }
+  });
+  });
+
